9195ca258c517bb0aaedc24cd518f74fe8815f7d
Ben_Alaya_Achraf <Ben_Alaya_Achraf@outlook.com>
2017-04-25 01:46:32 +0100

    Add project files.

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Gcm.Client;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+using Gcm.Client;
+
+namespace Onlinesession.Droid
+{
+	[Activity (Label = "Onlinesession.Droid",
+		Icon = "@drawable/icon",
+		MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+		Theme = "@android:style/Theme.Holo.Light")]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity , IAuthenticate
+	{
+       /* static MainActivity instance = null;*/
+        private MobileServiceUser user;
+
+        static MainActivity instance = null;
+
+        protected override void OnCreate(Bundle bundle)
+        {// Set the current instance of MainActivity.
+            instance = this;
+
+            base.OnCreate(bundle);
+
+            // Initialize Azure Mobile Apps
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+
+            // Initialize Xamarin Forms
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            App.Init((IAuthenticate)this);
+            // Load the main application
+            LoadApplication(new App());
+
+            //
+            try
+            {
+                // Check to ensure everything's set up right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+
+
+
+            //
+
+
+
+    }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
+        }
+
+
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+
+
+
+        //
+
+        //
+
+
+        public async Task<bool> Authenticate()
+        {
+            var success = false;
+            var message = string.Empty;
+            try
+            {
+                // Sign in with Facebook login using a server-managed flow.
+                user = await TodoItemManager.DefaultManager.CurrentClient.LoginAsync(this,
+                    MobileServiceAuthenticationProvider.Twitter);
+                if (user != null)
+                {
+                    message = string.Format("you are now signed-in as {0}.",
+                       user.UserId);
+                    success = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+
+            // Display the success or failure message.
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.SetMessage(message);
+            builder.SetTitle("Sign-in result");
+            builder.Create().Show();
+
+            return success;
+        }
+
+
+
+        // Create a new instance field for this activity.
+
+
+
+
+
+
+
+
+
+    }
+}
